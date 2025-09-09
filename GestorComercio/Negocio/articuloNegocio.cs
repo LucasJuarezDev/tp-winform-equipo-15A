c9d614f506be12913582419337e3971be9e2d312
LucasJuarezDev <lucasmartinjuarez5@gmail.com>
2025-09-09 07:28:15 -0300

    add dominio and negocio classes - arquitectura en capas

---
+++ GestorComercio/Negocio/articuloNegocio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio; // REFERENCIA A PROYECTO DOMINIO - ACORDARSE EN PROYECTO FRONT DE REFERENCIAR DOMINIO + NEGOCIO
+
+namespace Negocio
+{
+    public class articuloNegocio
+    {
+
+    }
+}
